ebcee6af63d8ad414a668be76cd05458bfc78c8d
ruthersmit B <bercyruthersmithadvid@gmail.com>
2022-01-02 19:18:49 -0500

    finished upgraded blog project

--- day_59_blog_upgrade/main.py
+++ day_59_blog_upgrade/main.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+from blog_data import data
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    blogs = data
+    return render_template("index.html", blogs=blogs)
 
 @app.route('/about')
 def about():
@@ -14,6 +16,10 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    return render_template('post.html', blog=data[post_id-1])
 
 
 if __name__ == '__main__':
