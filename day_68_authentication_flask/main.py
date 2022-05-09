767cbdc4b4f7a29f3f1eab75900dd4e0b8019911
ruthersmit B <bercyruthersmithadvid@gmail.com>
2022-05-09 13:48:05 -0400

    added auth and protected routes and password salting

--- day_68_authentication_flask/main.py
+++ day_68_authentication_flask/main.py
@@ -5,13 +5,23 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 
 app = Flask(__name__)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
 app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+@login_manager.user_loader
+def load_user(user_id):
+    print(user_id)
+    return User.query.get(int(user_id))
+
 ##CREATE TABLE IN DB
-class User(UserMixin, db.Model):
+
+class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
@@ -25,19 +35,48 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/register')
+@app.route('/register', methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    if request.method == "POST":
+        password = generate_password_hash(request.form.get('password'))
+        new_user = User(
+            email=request.form.get('email'),
+            name=request.form.get('name'),
+            password=password
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        print(request.form)
+        return redirect(url_for("secrets", name=new_user.name))
+    else:
+        return render_template("register.html")
 
 
-@app.route('/login')
+
+@app.route('/login', methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        # Find user by email entered.
+        user = User.query.filter_by(email=email).first()
+
+        if not user:
+            return f"user with email {email} not found"
+
+        if check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for("secrets", name=user.name))
+        else:
+            return f"Incorrect Password entered"
+
     return render_template("login.html")
 
 
-@app.route('/secrets')
-def secrets():
-    return render_template("secrets.html")
+@app.route('/secrets/<name>')
+@login_required
+def secrets(name):
+    return render_template("secrets.html", name=name)
 
 
 @app.route('/logout')
@@ -46,8 +85,9 @@ def logout():
 
 
 @app.route('/download')
+@login_required
 def download():
-    pass
+    return send_from_directory('static', filename="files/cheat_sheet.pdf")
 
 
 if __name__ == "__main__":
